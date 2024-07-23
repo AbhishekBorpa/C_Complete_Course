79d2a8a15af80d9127d4a576cb9f9ad844f76d91
Abhishek <abhishekborpa@gmail.com>
2024-07-23 18:03:50 +0530

    Commit

---
+++ Basics/Variable.c
@@ -0,0 +1,13 @@
+// var_type var_name = var_value
+// Memory Allocation and declaration
+// memory cell/ location address and name
+#include<stdio.h>
+int main(){
+  int a,b,c,d;
+  printf("hey ram \n");
+  a = 10;
+  b = 20;
+  c = a+b;
+  printf("sum of a and b is %d",c);
+  return 0;
+}
---
+++ Basics/datatype.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+  printf("hey buddy");
+  // data types
+  /* 
+  1. user-defined :- structure,uniun,enum,typedef
+  2. fundamental :- int, short , double , and char float void
+  3. derived : array, function , pointer
+
+
+  # Naming Conventions
+  1.camelCase
+  2.snake_case
+  3.kebab-case-naming
+  4. keep it short  and related
+  5.First letter should not be number or special characters
+   
+  
+  */
+
+
+
+  return 0;
+}
