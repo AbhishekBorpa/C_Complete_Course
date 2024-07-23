d6ee60e5bdb3f461e332df1e1ca2e62777a7d5ea
Abhishek <abhishekborpa@gmail.com>
2024-07-23 17:05:14 +0530

    NEW

--- Basics/Intro.c
+++ Basics/Intro.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
-    printf("hey Buddy");
+    printf("hey Buddy \n");
+    printf("This is new line");
+    // Comment mar di hai maine
+    /* hey you 
+    kya chahte ho
+    multi-line comment
+     chahiye kya */
     return 0;
 }
